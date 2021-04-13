2a4ef4ac4ae219d3ef508ce2fec16a45d0178bda
Terry Roddey <terry@t420.localdomain>
2021-04-13 13:07:27 -0400

    Add comments and google spreadsheet update

--- HTMLportfolio.py
+++ HTMLportfolio.py
@@ -5,26 +5,55 @@ import time
 import sys
 import re
 from bs4 import BeautifulSoup
+from oauth2client.service_account import ServiceAccountCredentials
 now = datetime.datetime.now()
-stocks = ['AAPL', 'MSFT']
 
-def portfolio_scraper(stocks):
+creds = ServiceAccountCredentials.from_json_keyfile_name("/home/terry/code/Stock-Investigator/PortfolioCredentials")
+client = gspread.authorize(creds)
+sheet = client.open("Portfolio").sheet1
+data = sheet.col_values(1)
+
+
+def portfolio_scraper(stocks, ss):
         portfolio_info = []
         for stock in stocks:
+
+            #Naming the URL string
             URL='https://finance.yahoo.com/quote/' + stock
+
+            #accessing the URL using BeuatifulSoup
             source = requests.get(URL)
             soup = BeautifulSoup(source.content, 'lxml')
+
+            #Acquiring the information using BeautifulSoup's "find" feature
             price = soup.find('span', attrs={"data-reactid": "50"}).text
-            price_string = '$' + price
             change = soup.find('span', attrs={"data-reactid": "51"}).text
             changelist = change.split(' ')
             percent_change = changelist[-1].replace('(', '').replace(')', '')
             dollar_change = changelist[0]
+
+            #Creating the stock dictionary object
             stock = {}
             stock['Price'] = price_string
             stock['Percent Change'] = percent_change
             stock['$ Change'] = dollar_change
+
+            #Adding the stock to the preliminary list
             portfolio_info.append(stock)
+
+            #This is for locating the areas of the google sheet where the data is going to be stored.
+            gs_ticker_location = ss.find(stock)
+            gs_dollar_value = ss.cell(gs_ticker_location.row, gs_ticker_location.col + 1)
+            gs_dollar_change = ss.cell(gs_ticker_location.row, gs_ticker_location.col + 2)
+            gs_percent_change = ss.cell(gs_ticker_location.row, gs_ticker_location.col + 3)
+
+            #Updating the information in the google sheet
+            gs_dollar_value.update(price)
+            gs_dollar_change.update(dollar_change)
+            gs_percent_change.update(percent_change)
+            
+        #Creating the entire portfolio as standard input (incase you want to use this in another program) (This is as a dictionary)
         portfolio = {stock:info for stock, info in zip(stocks, portfolio_info)}
-        print(portfolio)
-portfolio_scraper(stocks)
+        return portfolio
+portfolio_scraper(data, sheet)
+
